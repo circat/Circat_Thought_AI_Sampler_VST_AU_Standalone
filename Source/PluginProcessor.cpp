afff30079d6f638ea25eafccf18ab867ac9e9986
circat <erichlesovsky@googlemail.com>
2026-09-04 17:07:55 +0200

    Bump to Beta 0.8.3

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -5,7 +5,7 @@
  #include <CircatLog.h>
 #endif
 
-namespace { const char* kPluginLogName = "Circat Thought"; const char* kPluginVersion = "v0.8.2 beta"; }
+namespace { const char* kPluginLogName = "Circat Thought"; const char* kPluginVersion = "v0.8.3 beta"; }
 
 void circatLog (const juce::String& line)
 {
